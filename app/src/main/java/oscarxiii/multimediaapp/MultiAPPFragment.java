465c4bc9adc1750a6a452a5f93e0cd6e8bf7c019
oscar <46996403c@iespoblenou.org>
2016-02-18 08:53:42 +0100

    Añado al layout el boton para ver las notas

--- app/src/main/java/oscarxiii/multimediaapp/MultiAPPFragment.java
+++ app/src/main/java/oscarxiii/multimediaapp/MultiAPPFragment.java
@@ -15,8 +15,7 @@ public class MultiAPPFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         /*View view = inflater.inflate(R.layout.fragment_multi_app, container, false);
         Firebase.setAndroidContext(getContext());
